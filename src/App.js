859d8d231e2203147ae53906a14fdf1c80da2eb9
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2019-09-25 12:27:48 -0400

    Passing Props

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
-import React from 'react';
-import './App.css';
-import {Route} from "react-router-dom";
-import Top3 from "./Top3Page"
-import ListOfTen from './components/listOfTen'
-import './styles/Top3.css'
-import Login from './components/Login'
-import SignUp from './components/SignUp';
-import LandingPage from './components/LandingPage';
-import './styles/ActivityPrompt.css'
-import FormikActivityForm from "./components/ActivityPrompt"
-import NavBar from './components/NavBar';
-import Top20List from './components/Top20List'
-import './styles/Top20.css'
-import Compare from './components/Compare'
-
-
+import React from "react";
+import "./App.css";
+import { Route } from "react-router-dom";
+import Top3 from "./Top3Page";
+import ListOfTen from "./components/listOfTen";
+import "./styles/Top3.css";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import LandingPage from "./components/LandingPage";
+import "./styles/ActivityPrompt.css";
+import FormikActivityForm from "./components/ActivityPrompt";
+import NavBar from "./components/NavBar";
+import Top20List from "./components/Top20List";
+import "./styles/Top20.css";
+import Compare from "./components/Compare";
 
 function App() {
+  const data = [
+    { value: "Athleticism", priority: false, user_id: null },
+    { value: "Art", priority: false, user_id: null },
+    { value: "Literature", priority: false, user_id: null },
+    { value: "Creativity", priority: false, user_id: null },
+    { value: "Making a difference", priority: false, user_id: null },
+    { value: "Independence", priority: false, user_id: null },
+    { value: "Kindness", priority: false, user_id: null },
+    { value: "Generosity", priority: false, user_id: null },
+    { value: 'Carpe diem, "Seize the day"', priority: false, user_id: null },
+    { value: "Community", priority: false, user_id: null },
+    { value: "Music", priority: false, user_id: null },
+    { value: "Morals", priority: false, user_id: null },
+    { value: "Environmentalism", priority: false, user_id: null },
+    { value: "Relationships", priority: false, user_id: null },
+    { value: "Humor", priority: false, user_id: null },
+    { value: "Successful Career", priority: false, user_id: null }
+  ];
+
   return (
     <div>
-    <NavBar/>
-    <Route exact path="/" component={LandingPage}/>  
-    <Route path="/login" component={Login} />
-    <Route path="/register" component={SignUp} />
-    <Route path="/allvalues" component={Top20List} />
-    <Route path="/listoften" component={ListOfTen} />
-    <Route path="/top3" component={Top3} />
-    <Route path="/projects" component={FormikActivityForm} />
-    <Route path="/compare" component={Compare} />
-
+      <NavBar />
+      <Route exact path="/" render={props => <LandingPage data={data} />} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={SignUp} />
+      <Route path="/allvalues" render={props => <Top20List data={data} />} />
+      <Route path="/listoften" render={props => <ListOfTen data={data} />} />
+      <Route path="/top3" render={props => <Top3 data={data} />} />
+      <Route path="/projects" component={FormikActivityForm} />
+      <Route path="/compare" render={props => <Compare data={data} />} />
     </div>
   );
 }
